b2c34aecd46698785aa63394e25d3bcd37e25e41
Artem Pasko <guramibox777@gmail.com>
2022-12-26 16:00:15 +0300

    feat: add entity package

---
+++ module-repository/src/main/java/com/mjc/school/entity/News.java
@@ -0,0 +1,77 @@
+package com.mjc.school.entity;
+
+import java.time.LocalDateTime;
+
+public class News {
+
+    private static Long increment = 0L;
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createDate;
+    private LocalDateTime lastUpdateDate;
+    private Long authorId;
+
+    public News() {
+        increment++;
+        id = increment;
+    }
+
+    public News(Long id, String title, String content, LocalDateTime createDate, LocalDateTime lastUpdateDate, Long authorId) {
+        increment = id;
+        this.id = increment;
+        this.title = title;
+        this.content = content;
+        this.createDate = createDate;
+        this.lastUpdateDate = lastUpdateDate;
+        this.authorId = authorId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+}
